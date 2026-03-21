18ef931160a9609e922ac3cb7cc3f991a052e01d
prathmesh-ka-github <pkale@albany.edu>
2026-03-21 16:25:08 -0400

    :recycle: refactor(server) moved api calls to server & exported login functions

--- server/scripts/login.js
+++ server/scripts/login.js
@@ -17,100 +17,7 @@ const { error } = require("console")
 const connectionstring = process.env.DB_CONNECTION_STRING;
 mongoose.connect(connectionstring)
 
-// Hashing a password
-bcrypt.hash(myPassword, saltRounds, function(err, hash) {
-    console.log(myPassword);
-    encriptedpass = hash
-});
-
-// Comparing a password
-bcrypt.compare(myPassword, encriptedpass, function(err, result) {
-    console.log(result);
-    
-});
-
 // We have to structure the User data after that we can properly alter these functions. We have to do DB arch design first.
-
-
-app.post('/login', async(req,res,next) => {
-    const user = req.body;
-    // console.log(user)
-    if (await searchUser(user)) {
-        try {
-            const dbuser = await User.findOne({email : user.email})
-            let checkpass = bcrypt.compareSync(user.password, dbuser.password)
-            if (checkpass) {
-                var token = jwt.sign({id : dbuser._id}, 'secretkey');
-                await User.updateOne({email: user.email}, {$set:{token: token}})
-                console.log("successfully logged in!")
-                res.redirect('/')
-            }
-            else {
-                throw "ERR - Invalid credentials. Wrong password."
-            }
-        } catch (err) {
-            res.status(401).json({
-                "err":"ERR - Invalid Credentials! Try again or head to signin.",
-                "code":401
-            })
-            console.log(err)
-        }
-    } else {
-        res.status(400).json({
-            "err":"ERR - Invalid Credentials! Try again or head to signin.",
-            "code":400
-        })
-    }
-    next();
-})
-
-    app.post('/register', async (req,res,next) => {
-        try {
-            const user = req.body;
-            let result = await checkUser(user)
-            if(result) {
-                user.token = "none"
-                console.log("User not found...Creating new user.")
-                addUser(user)   
-                res.status(201)
-                res.redirect('/login')
-            }
-            else {
-                console.log("ERR - user found")
-                // res.redirect('/signin')
-                // 400 - Bad request. Error from client side.
-                res.status(400).json({
-                    "err":"ERR - User already exists! Try again or head to login.",
-                    "code":400
-                })
-            }
-            next();
-        } catch (error) {
-            console.error(error);
-        }
-    })
-
-app.get('/users', async (req,res) => {
-    try {
-        let data = await User.find({}, {_id : true})
-        res.status(200).json(data);
-    } catch (err) {
-        console.error(err)
-    }
-})
-
-app.post('/getuserdetails', async (req,res) => {
-    try {
-        console.log(req.body)
-        const useremail = req.body.useremail
-        // console.log('getuserdetails api called')
-        const user = await User.findOne({ email: useremail });
-        // console.log(user)
-        res.json(user)
-    } catch (err) {
-        console.error(err)
-    }
-})
 
 async function checkUser(inputuser) {
     try {
@@ -151,3 +58,5 @@ async function searchUser(inputuser) {
         console.error(err)
     }
 }
+
+module.exports = { addUser, searchUser, checkUser };
--- server/server.js
+++ server/server.js
@@ -1,9 +1,10 @@
 require('dotenv').config({ path: '.env.development' });
 const express = require('express');
-// We will use cors later
-// const cors = require("cors")
 const app = express();
 const port = process.env.PORT;
+
+// We will use cors later
+// const cors = require("cors")
 
 app.use(express.json())
 app.use(express.urlencoded({extended : false}));
@@ -12,9 +13,91 @@ const { error } = require("console")
 const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
+const auth = require('./scripts/login');
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.post('/login', async(req,res,next) => {
+    const user = req.body;
+    // console.log(user)
+    if (await auth.searchUser(user)) {
+        try {
+            const dbuser = await User.findOne({email : user.email})
+            let checkpass = bcrypt.compareSync(user.password, dbuser.password)
+            if (checkpass) {
+                var token = jwt.sign({id : dbuser._id}, 'secretkey');
+                await User.updateOne({email: user.email}, {$set:{token: token}})
+                console.log("successfully logged in!")
+                res.redirect('/')
+            }
+            else {
+                throw "ERR - Invalid credentials. Wrong password."
+            }
+        } catch (err) {
+            res.status(401).json({
+                "err":"ERR - Invalid Credentials! Try again or head to signin.",
+                "code":401
+            })
+            console.log(err)
+        }
+    } else {
+        res.status(400).json({
+            "err":"ERR - Invalid Credentials! Try again or head to signin.",
+            "code":400
+        })
+    }
+    next();
+})
+
+app.post('/register', async (req,res,next) => {
+    try {
+        const user = req.body;
+        let result = await auth.checkUser(user)
+        if(result) {
+            user.token = "none"
+            console.log("User not found...Creating new user.")
+            auth.addUser(user)   
+            res.status(201)
+            res.redirect('/login')
+        }
+        else {
+            console.log("ERR - user found")
+            // res.redirect('/signin')
+            // 400 - Bad request. Error from client side.
+            res.status(400).json({
+                "err":"ERR - User already exists! Try again or head to login.",
+                "code":400
+            })
+        }
+        next();
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+app.get('/users', async (req,res) => {
+    try {
+        let data = await User.find({}, {_id : true})
+        res.status(200).json(data);
+    } catch (err) {
+        console.error(err)
+    }
+})
+
+app.post('/getuserdetails', async (req,res) => {
+    try {
+        console.log(req.body)
+        const useremail = req.body.useremail
+        // console.log('getuserdetails api called')
+        const user = await User.findOne({ email: useremail });
+        // console.log(user)
+        res.json(user)
+    } catch (err) {
+        console.error(err)
+    }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
