11ac62e95932e988658ec0cf2f9a91b63328193a
prathmesh-ka-github <pkale@albany.edu>
2026-04-12 12:28:07 -0400

    :alembic: test(client) Socket testing

--- client/src/layouts/LandingPage.tsx
+++ client/src/layouts/LandingPage.tsx
@@ -3,8 +3,15 @@ import MainSection from '../components/Mainsection';
 import KnowMore from '../components/Knowmore';
 import FAQs from '../components/Faqs';
 import Footer from '../components/Footer';
+import { io } from "socket.io-client"
 
 export default function LandingPage() {
+  // const server = import.meta.env.VITE_SERVER_URL;
+  // const socket = io(server)
+  // socket.on('connect',()=> {
+  //   console.log(`You connected to the socket!!! ${socket.id}`)
+  // })
+
   return (
     <div className="min-h-screen w-full bg-white">
       <Navbar />
